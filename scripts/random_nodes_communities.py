298b1db16d9f28d431e5640d89a15d88ae6c7417
Fawaz Dabbaghie <fawaz@Fawazs-MacBook-Air.local>
2025-12-04 11:18:20 +0900

    some updates and testing the on-disk hash

--- scripts/random_nodes_communities.py
+++ scripts/random_nodes_communities.py
@@ -1,17 +1,29 @@
-import os
 import random
 import string
+import sys
 
 
+if len(sys.argv) < 4:
+    print("You need to give the output file and the number of strings and the number of queries>")
+    exit(1)
 
+n_communities = int(sys.argv[2])
+n_queries = int(sys.argv[3])
 
 communities = []
 for i in range(1500):
     communities.append(i)
 
+queries = []
 characters = string.ascii_letters + string.digits
 
-with open('random_nodes_communities.txt', 'w') as outfile:
-    for _ in range(50000):
+choices = [True, False
+           ]
+with open(sys.argv[1], 'w') as outfile:
+    for _ in range(n_communities):
         random_string = ''.join(random.choice(characters) for i in range(9))
+        if random.choice(choices):
+            queries.append(random_string)
+            if len(queries) == n_queries:
+                choices = [False]
         outfile.write(random_string + " " + str(random.choice(communities)) + "\n")
