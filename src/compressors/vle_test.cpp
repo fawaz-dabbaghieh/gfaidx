9c38dbb67f4997691455f4a0c99c518a306b0348
Fawaz Dabbaghie <fawaz.dabbaghieh@gmail.com>
2025-12-26 21:49:42 +0900

    structure changes to vle_test to work on the PGGB graph, now goes through all the L lines first, then goes again for the P lines

--- src/compressors/vle_test.cpp
+++ src/compressors/vle_test.cpp
@@ -30,19 +30,28 @@ int main(int argc, char** argv) {
     Reader r;
     r.open(argv[1]);
     std::string_view line;
-    int n_paths = 0;
-
-    std::ofstream out(argv[2], std::ios::binary);
-
     std::unordered_map<std::string, unsigned int> node_id_map;
+    long int progress_counter = 0;
     while (r.read_line(line)) {
         // I know that all the L lines will be first
         if (line[0] == 'L') {
-            auto [fst, snd] = extract_L_nodes(line);
+            progress_counter++;
+            if (progress_counter % 5000000 == 0) {
+                std::cout << "Number of L lines processed: " << progress_counter << std::endl;
+            }
+
+            auto [first, second] = extract_L_nodes(line);
             unsigned int src, dest;
-            get_int_node_id(node_id_map, fst, src);
-            get_int_node_id(node_id_map, snd, dest);
+            get_int_node_id(node_id_map, first, src);
+            get_int_node_id(node_id_map, second, dest);
         }
+    }
+    r.close();
+
+    int n_paths = 0;
+    std::ofstream out(argv[2], std::ios::binary);
+    r.open(argv[1]);
+    while (r.read_line(line)) {
         if (line[0] == 'P') {
             n_paths++;
             std::string path_name;
@@ -58,9 +67,13 @@ int main(int argc, char** argv) {
             out.write(&separator, 1);
             out.write(reinterpret_cast<const char*>(encoded_path.data()), encoded_path.size());
             out.write(&separator, 1);
+            if (n_paths % 1000 == 0) {
+                std::cout << "Number of paths processed: " << n_paths << std::endl;
+            }
         }
     }
     out.close();
+
     std::cout << "Number of paths compressed: " << n_paths << std::endl;
 
     return 0;
